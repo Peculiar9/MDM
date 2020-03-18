3e9b5e4f01e7ea42a673a75288c1184f04b1bb07
Peculiar.NET <31594432+Peculiar9@users.noreply.github.com>
2020-03-18 01:32:00 +0100

    "Homepage" "AdminPage" "LoginforUser"
    They are linked to the online bootstrap i.e CDN and their images and logo are there.
    But I still think the JS LINK isn't there as i noticed with the previous commits. Check it out thanks.

---
+++ admin/admin_dashboard/script.js
@@ -0,0 +1,27 @@
+
+
+// document.querySelectorAll('#delete').forEach(item => {item.addEventListener('click', event => {
+    
+//     window.alert('Do you want to delete this Product?');
+//     document.querySelector('#last-row').style.display = "none";
+    
+// })});
+
+
+
+rowsection = document.getElementById('row');
+
+    
+
+document.querySelectorAll('#delete1').forEach(item => {item.addEventListener('click', event => {
+    const tablerow = rowsection.childElementCount;
+    var i = document.querySelectorAll('#delete1').length;
+    
+
+
+    rowsection.removeChild(rowsection.children[i]);
+    // window.alert('click');
+    console.log('click');
+
+    
+})});
