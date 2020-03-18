4ea28f8097d33ece333c5e5bc9fd86eec5fcc6dc
Peculiar.NET <31594432+Peculiar9@users.noreply.github.com>
2020-03-18 01:17:09 +0100

    This is the index page and the bootstrap file.
    The bootstrap file is not necessary but you can use the cdn or the main file. Which will probably be on your machine

---
+++ script.js
@@ -0,0 +1,43 @@
+
+
+// document.querySelector('#details').addEventListener('click', function(){
+//     location.href = "../Products_details/products_details.html";
+// });
+
+
+    
+// document.querySelector('#details').addEventListener('click', function(){
+   
+//     document.querySelectorAll('#details');
+
+//     for (var i = 0; i < details.length; i++){
+//         location.href = "../Products_details/products_details.html";
+//     }
+    
+// });
+
+// document.querySelectorAll('#details');
+
+// details.forEach(details => {
+//     location.href = "../Products_details/products_details.html";
+// });
+
+document.querySelectorAll('#details').forEach(item => {item.addEventListener('click', event => {
+    location.href = "../Products_details/products_details.html";
+    demol.textContent = "welcome";
+})});
+
+var demol = document.getElementById('demo');
+
+
+// document.querySelector('#shirt-cart-button').addEventListener('click', function(){
+//     document.querySelector('.whole-nav').style.opacity="0.2";
+
+// });
+
+
+// document.querySelector('#home').addEventListener('click', function(){
+//         document.querySelector('.whole-nav').style.opacity="1";
+    
+// });
+
