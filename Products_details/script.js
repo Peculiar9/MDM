7ef46af961cff330feba0853c1d41940e136e746
Peculiar.NET <31594432+Peculiar9@users.noreply.github.com>
2020-03-18 01:45:43 +0100

    "Products", "Assets", "Registration", "ProductD.."
    Product details. And some images for the website and some other stuff. 
    There is also the page for the registration
    Hope it is PHP you are using for the backend i can write some php for the user auth and verification if you dont mind so you can join with the end points and make the required changes sir

---
+++ Products_details/script.js
@@ -0,0 +1,13 @@
+var addtocart;
+var cartcount;
+
+addtocart = document.querySelector('#add-to-cart');
+cartcount = document.querySelector('#cart-count');
+cartcount = 0
+
+
+addtocart.addEventListener('click', function(){
+    cartcount = cartcount + 1;
+    console.log(cartcount);
+    document.querySelector('#cart-count').textContent = cartcount;
+})
---
+++ Registration/script.js
@@ -0,0 +1,12 @@
+
+
+document.querySelector('.regbtn').addEventListener('click', function(){
+    if (document.querySelector('#password1').value !== document.querySelector('#password2').value){
+        document.querySelector('#alert').textContent = "Password must match each other";
+        document.querySelector('#alert').style.color = "red";
+    }else{
+        document.querySelector('#alert').style.display = "none";
+    }
+
+
+});
